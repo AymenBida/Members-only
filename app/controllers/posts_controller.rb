d31517a7ab2842f961e08366cd0aaeeba6c7ea95
AymenBida <aymen.bida.00@gmail.com>
2021-02-16 11:01:16 +0100

    Add a post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
 class PostsController < ApplicationController
 
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(posts_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :body)
   end
 end
