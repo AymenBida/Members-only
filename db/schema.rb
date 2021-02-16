f97636c3f5a971708e2864f5d34a0aeca1c2612c
AshakaE <ashakaegerega@gmail.com>
2021-02-16 10:30:17 +0100

    Sign in and out feature

--- db/schema.rb
+++ db/schema.rb
@@ -27,11 +27,9 @@ ActiveRecord::Schema.define(version: 2021_02_15_131125) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "users"
