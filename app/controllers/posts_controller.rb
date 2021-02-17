e491b51f21524eacebec6dbb0e1321255a859b2c
AymenBida <aymen.bida.00@gmail.com>
2021-02-17 10:05:49 +0100

    Add before_action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def new
     @post = current_user.posts.build
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,3 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, :body, presence: true, length: { maximum: 280 }
 end
-
